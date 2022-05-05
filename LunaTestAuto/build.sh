e8207838623f29a2d49cf529a6267d41580263fe
Chengyiyu <ppoolkiu@gmail.com>
2022-05-05 09:23:34 +0800

    Update build.sh

--- LunaTestAuto/build.sh
+++ LunaTestAuto/build.sh
@@ -1,5 +1,7 @@
-chmod +x geckodriver
-chmod 777 run.sh
+SH_PATH=$(dirname $(readlink -f $0))
+
+chmod +x /$SH_PATH/geckodriver
+chmod 777 /$SH_PATH/run.sh
 mv geckodriver /usr/local/bin/
 apt-get install ipmitool
-pip3 install -r requirements.txt
+pip3 install -r /$SH_PATH/requirements.txt
