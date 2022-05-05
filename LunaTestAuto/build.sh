bcf2a486b6adae3e167c40af92deb2b068a698d8
Chengyiyu <ppoolkiu@gmail.com>
2022-05-05 10:46:57 +0800

    Update build.sh

--- LunaTestAuto/build.sh
+++ LunaTestAuto/build.sh
@@ -1,7 +1,18 @@
-SH_PATH=$(dirname $(readlink -f $0))
-
-chmod +x /$SH_PATH/geckodriver
-chmod 777 /$SH_PATH/run.sh
+#!/bin/bash
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     echo "$DIR"
+}
+chmod +x $(get_script_dir)/geckodriver
+chmod 777 $(get_script_dir)/run.sh
 mv geckodriver /usr/local/bin/
 apt-get install ipmitool
-pip3 install -r /$SH_PATH/requirements.txt
+pip3 install -r $(get_script_dir)/requirements.txt
